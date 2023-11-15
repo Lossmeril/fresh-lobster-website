356ad8329c7bb7b3eec2cc2f065bd883704b23aa
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2023-11-15 14:47:25 +0100

    style: header links and button

--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 import Button from "./button";
+
+const links = [
+  ["/", "Domů"],
+  ["/o-nas", "O nás"],
+  ["/projekty", "Projekty"],
+  ["/kontakt", "Kontakt"],
+];
 
 const Header = () => {
   return (
     // DESKTOP NAVBAR
-    <nav className="navbar w-screen h-24 fixed bg-[#ffffff] shadow-lg">
+    <nav className="navbar w-screen h-24 fixed bg-white shadow-sm">
       <div className="flex flex-row gap-6 h-full justify-between items-center mx-[10vw]">
         <div className="w-[118px] relative aspect-[12/5]">
           <a href="/">
@@ -17,10 +25,16 @@ const Header = () => {
           </a>
         </div>
         <div className="flex flex-row gap-12 h-full justify-center items-center">
-          <p>Domů</p>
-          <p>O nás</p>
-          <p>Projekty</p>
-          <p>Kontakt</p>
+          {links.map((link) => (
+            // <Link key={link[0]} href={link[0]}>
+            <Link
+              key={link[0]}
+              href=""
+              className="font-medium text-lg rotate-hover hover:text-lobster"
+            >
+              <p>{link[1]}</p>
+            </Link>
+          ))}
         </div>
 
         <Button link="/" inverse={false}>
